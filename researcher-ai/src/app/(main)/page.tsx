edf32bd66770676b73212a8ebbbaff2107dd980f
BKunk10 <brady.kunkle@gmail.com>
2025-08-04 12:31:49 -0400

    no per

--- researcher-ai/src/app/(main)/page.tsx
+++ researcher-ai/src/app/(main)/page.tsx
@@ -1,7 +1,7 @@
 export default function DashboardPage() {
   return (
     <div className="text-2xl font-semibold">
-      Welcome to ReSearcher AI.
+      Welcome to ReSearcher AI
     </div>
   );
 }
